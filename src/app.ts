a67d0921aa250ac76ed9cbce2bc17f0a138caadb
Hung_To <tanh14226@gmail.com>
2025-05-13 23:06:43 +0700

    Template Engine

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import 'dotenv/config';
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send("Hello Hung To");
+    res.render("home");
 });
 
 app.get("/about", (req, res) =>{
